d50e6353352e997ade5009b550ae1e0ef8b28dac
Zawitkowski <46582567+Zawitkowski@users.noreply.github.com>
2019-05-16 17:51:51 -0700

    Update ADVICE from Diala.py

--- ADVICE from Diala.py
+++ ADVICE from Diala.py
@@ -1,14 +1,17 @@
 import scikitlearn
 
-#print the letters in the word red, randomly 10 times
-import random
-Word = input('Word is:')
-for i in range(10):
-    position= random.randrange(-len(Word), len(Word))
-    print(Word[position])
+#Commonly used packages by data scientists
 
 #Pandas / Pd / Pd.XxXxxx (using the pandas package)
 
 #Scikitlearn --> Machine learning
 
 #numpy --> math manipulation
+
+#Practice
+#print the letters in the word red, randomly 10 times
+import random
+Word = input('Word is:')
+for i in range(10):
+    position= random.randrange(-len(Word), len(Word))
+    print(Word[position])
